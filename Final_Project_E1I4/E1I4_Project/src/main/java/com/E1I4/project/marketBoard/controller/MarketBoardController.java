6632bad64806a99d75559560328ea671f427b2b0
jiho <wlgj0802@naver.com>
2022-12-05 16:40:58 +0900

    마켓 뷰

--- Final_Project_E1I4/E1I4_Project/src/main/java/com/E1I4/project/marketBoard/controller/MarketBoardController.java
+++ Final_Project_E1I4/E1I4_Project/src/main/java/com/E1I4/project/marketBoard/controller/MarketBoardController.java
@@ -1,8 +1,20 @@
 package com.E1I4.project.marketBoard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.E1I4.project.marketBoard.model.service.MarketBoardService;
 
 @Controller
 public class MarketBoardController {
 
+	@Autowired
+	private MarketBoardService mkService;
+	
+	@RequestMapping("marketBoardList.ma")
+	public String marketBoardList() {
+		return "marketBoardList";
+	}
+	
 }
